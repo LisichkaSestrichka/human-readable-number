158137b495302a36c64cc9b52b90da068901f5e1
LisichkaSestrichka <79520443+LisichkaSestrichka@users.noreply.github.com>
2021-03-14 22:19:17 +0300

    init: started human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+  let numb = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+  let twoNumb = ['', '', '', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  if (number >= 0 && number < 20){
+    return numb[number];
+  }
+  if (number == 30 || number == 40 || number == 50 || number == 60 || number == 70 || number == 80 || number == 90);
+    return twoNumb[number];
 }
